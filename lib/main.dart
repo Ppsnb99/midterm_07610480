199a2d72a27aa94fe9d1d9d69e77227bb8397676
siriporn nabangchang <nabangchang_s2@silpakorn.edu>
2022-01-16 12:00:09 +0700

    edit2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,47 +9,112 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Flutter Demo',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // primarySwatch: Colors.indigo,  //title
+
+        primarySwatch: Colors.pink,
       ),
       home: HomePage(),
     );
   }
 }
-
-
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  final _controller = TextEditingController();
-
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/bg.jpg'),
-              fit: BoxFit.cover,
-            ),
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(height: 50.0,),
+              Text('PIG WIGHT',
+                style: TextStyle(fontSize: 50.0, color: Colors.pink,) ,
+              ),
+              Text("CALCULATOR",
+                style: TextStyle(fontSize: 30.0, color: Colors.pink,) ,
+              ),
+              Container(height: 30.0,),
+              Image.asset('assets/images/pig.png',width: 350.0,),
+              Container(height: 20.0,),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: const Offset(3.0, 5.0),
+                                  blurRadius: 5.0,
+
+                              ),
+                            ]
+
+                        ),
+                        child: Column(
+                          children: [
+                            Text('LENGHT'),
+                            Text('(cm)'),
+                            TextField(
+                              textAlign: TextAlign.center,
+
+                            ),
+                            Container(height: 20.0,)
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(width: 20.0,),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: const Offset(3.0, 5.0),
+                                  blurRadius: 5.0,
+
+                              ),
+                            ]
+
+                        ),
+
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(height: 20,),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text('CALCULATE'))
+            ],
           ),
 
-      SizeBox(height: 30.0),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center, //ปรับตำแหน่ง
-                children: [
-                  Padding(
-                    
-                  )
-                ]
-            )
-          )
-        );
+        ),
+
+      ),
+    );
   }
 }
 
